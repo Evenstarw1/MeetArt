69ff4e6318318d2d18e24a25306cd00894656666
Evenstarw1 <lauracorrallopez@gmail.com>
2018-10-18 17:58:05 +0200

    Heroku

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ const cors = require('cors');
 
 
 mongoose
-  .connect('mongodb://localhost/back', {useNewUrlParser: true})
+  .connect(process.env.DBURL, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
